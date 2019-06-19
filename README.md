064eec5ec22839f16820df147e4485f547441bb3
jaycotton <lbmgmusic@gmail.com>
2019-06-18 17:15:00 -0700

    Fix bugs in toupper change the readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # toupper
 convert and fix files to upper case for cp/m asm program
 
-For now, just leave a file XYZZY.TMP on the drive.  Later after we
-are happy with the code, I will have it remove the input file, and rename
-the temp file back to the input file name.   But better save than sorry....
-Maybe we will have xyzzy.tmp be the final destination for the input file....hmmmm
+NOTE:  This program reads an input file, processes it, and removes the input
+file and replaces it with a new file.  I HIGHLY RECOMMEND that you make a copy
+of any file that you simply can't live without, since there is a possibility 
+of error causeing the loss of your data.
+
+
 
--- main.c
+++ main.c
@@ -1,37 +1,61 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 FILE *in;
-FILE *out;
 FILE *tmp;
 char infilename[30];
-char outfilename[30];
 char tmpfilename[30];
-char c;
+int c;
 char *p;
-int main(int argc, char *argv[])
+char bigbuf[4096];
+int rz;
+int i;
+int
+main (int argc, char *argv[])
 {
-	memset(infilename,0,80);
-	memset(outfilename,0,80);
-	memset(tmpfilename,0,80);
-	strcpy(infilename,argv[1]);
-	strcpy(outfilename,argv[1]);
-	strcpy(tmpfilename,"C:XYZZY.TMP");
+  memset (infilename, 0, 80);
+  memset (tmpfilename, 0, 80);
+  strcpy (infilename, argv[1]);
+  strcpy (tmpfilename, "XYZZY.TMP");
 
-	in=fopen(infilename,"r");
-	tmp = fopen(tmpfilename,"a");
-	if(tmp == 0){
-		printf("can open tmpfile");
-		exit(0);
+  in = fopen (infilename, "r");
+  tmp = fopen (tmpfilename, "a");
+  if (tmp == 0)
+    {
+      printf ("can open tmpfile");
+      exit (0);
+    }
+// see what we can do to speed this up a lot.
+//      while((c=fgetc(in))!=EOF){
+//              if(c == '\n'){
+//                      fputc(0x0d,tmp);
+//              }else
+//                      fputc(toupper(c),tmp);
+//      }
+  do
+    {
+      rz = 0;
+      memset (bigbuf, 0, 4096);
+      rz = fread (bigbuf, 1, 4096, in);
+      printf ("read record, size = %d\n", rz);
+      if (rz)
+	{
+	  for (i = 0; i < 4096; i++)
+	    {
+	      c = toupper (bigbuf[i]);
+	      if (c == '\n')
+		fputc (0x0d, tmp);
+	      else if(c != 0)
+		fputc (c, tmp);
+	    }
 	}
-	while((c=fgetc(in))!=EOF){
-		if(c == '\n'){
-			fputc(0x0d,tmp);
-		}else
-			fputc(toupper(c),tmp);
-	}
-	fflush(tmp);
-	fclose(in);
-	fclose(tmp);
+    }
+  while (rz != 0);
+  fflush (tmp);
+  fclose (in);
+  fclose (tmp);
+  remove (infilename);
+  rename (tmpfilename, infilename);
 }
